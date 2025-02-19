b97d518fbb18028e9b6a011719dd24d8c118a4fe
Peter Rigali <peterjrigali@gmail.com>
2025-02-19 13:08:33 -0600

    Added uni file

---
+++ universal.py
@@ -0,0 +1,5 @@
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+REFRESH = ""
+BASE_URL = "https://www.strava.com/"
+ACTIVITY_UPDATES = {'commute': bool, 'trainer': bool, 'hide_from_home': bool, 'description': str, 'name': str, 'type': str, 'sport_type': str, 'gear_id': str}
